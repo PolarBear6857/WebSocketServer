a5087e820c49ded7644b8b2b9cef64d259de7654
PolarBear6857 <84445993+PolarBear6857@users.noreply.github.com>
2023-10-15 13:56:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ class Message():
         self.message = message
 
     def __str__(self):
-        return f"({self.client_id}, {self.message}"
+        return f"({self.client_id}, {self.message})"
 
 
-banned_clients = set()  # Seznam zakázaných uživatelů
+banned_clients = set()  # Set of banned clients
 connected = {}
 next_client_id = 1
 messages = []
@@ -22,7 +22,9 @@ async def handle_client(websocket, path):
     client_id = next_client_id
     next_client_id += 1
 
-    if websocket.remote_address[0] in banned_clients:
+    client_ip = websocket.remote_address[0]
+
+    if client_ip in banned_clients:
         await websocket.send("You are banned from the server.")
         await websocket.close()
         return
@@ -38,6 +40,11 @@ async def handle_client(websocket, path):
                 if id != client_id:
                     await client.send(f"Client {client_id}: {message}")
 
+            if "rum" in message.lower():  # Check for "rum" or "Rum" in the message
+                banned_clients.add(client_ip)
+                await websocket.send("You have been banned for using the word 'rum'.")
+                await websocket.close()
+
     except websockets.exceptions.ConnectionClosedError:
         pass
 
@@ -46,7 +53,7 @@ async def handle_client(websocket, path):
         print(f"Client {client_id} disconnected")
 
 
-start_server = websockets.serve(handle_client, "localhost", 8765)
+start_server = websockets.serve(handle_client, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
